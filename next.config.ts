580e358eb76a0b7d02c210cec4e65d382811f132
Josh Roy <10731363+JRoy@users.noreply.github.com>
2025-03-06 14:36:34 -0500

    add redirects from old site

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,93 @@ const nextConfig: NextConfig = {
     pageExtensions: ['ts', 'tsx', 'mdx', 'md'],
     redirects: async () => ([
         {
+            source: '/community.html',
+            destination: '/community',
+            permanent: true
+        },
+        {
+            source: '/discord.html',
+            destination: '/discord',
+            permanent: true
+        },
+        {
+            source: '/downloads.html',
+            destination: '/downloads',
+            permanent: true
+        },
+        {
+            source: '/dump.html',
+            destination: '/dump',
+            permanent: true
+        },
+        {
             source: '/wiki',
             destination: '/wiki/introduction',
+            permanent: true
+        },
+        {
+            source: '/wiki/Home.html',
+            destination: '/wiki/introduction',
+            permanent: true
+        },
+        {
+            source: '/wiki/BannerMeta.html',
+            destination: '/wiki/banner-meta',
+            permanent: true
+        },
+        {
+            source: '/wiki/Color-Permissions.html',
+            destination: '/wiki/color-permissions',
+            permanent: true
+        },
+        {
+            source: '/wiki/Command-Cooldowns.html',
+            destination: '/wiki/command-cooldowns',
+            permanent: true
+        },
+        {
+            source: '/wiki/Discord-Tutorial.html',
+            destination: '/wiki/discord',
+            permanent: true
+        },
+        {
+            source: '/wiki/Discord-Link.html',
+            destination: '/wiki/discord-link',
+            permanent: true
+        },
+        {
+            source: '/wiki/GeoIP.html',
+            destination: '/wiki/geo-ip',
+            permanent: true
+        },
+        {
+            source: '/wiki/Improvements.html',
+            destination: '/wiki/improvements',
+            permanent: true
+        },
+        {
+            source: '/wiki/Installing-EssentialsX.html',
+            destination: '/wiki/installing',
+            permanent: true
+        },
+        {
+            source: '/wiki/Keywords.html',
+            destination: '/wiki/keywords',
+            permanent: true
+        },
+        {
+            source: '/wiki/Locale.html',
+            destination: '/wiki/translations',
+            permanent: true
+        },
+        {
+            source: '/wiki/Module-Breakdown.html',
+            destination: '/wiki/modules',
+            permanent: true
+        },
+        {
+            source: '/wiki/Text-Commands.html',
+            destination: '/wiki/text-commands',
             permanent: true
         }
     ]),
@@ -20,9 +105,9 @@ const nextConfig: NextConfig = {
 
 const withMDX = createMDX({
     options: {
-        // @ts-ignore - this is required for this to work with turbopack
+        // @ts-expect-error - this is required for this to work with turbopack
         remarkPlugins: [['remark-gfm', {strict: true, throwOnError: true}]],
-        // @ts-ignore - this is required for this to work with turbopack
+        // @ts-expect-error - this is required for this to work with turbopack
         rehypePlugins: [['rehype-slug', {strict: true, throwOnError: true}]]
     }
 });
