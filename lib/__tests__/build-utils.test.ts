cbda47a866b22fab596603f72b7b96039106e527
Josh Roy <10731363+JRoy@users.noreply.github.com>
2025-06-04 12:53:33 -0700

    feat: improve commands page

--- lib/__tests__/build-utils.test.ts
+++ lib/__tests__/build-utils.test.ts
@@ -1,30 +1,38 @@
-import { getVersionFromArtifact, getModuleIdFromArtifact, getCommitIdFromArtifact } from '../build-utils';
+import {
+  getCommitIdFromArtifact,
+  getModuleIdFromArtifact,
+  getVersionFromArtifact,
+} from '../build-utils';
 
 describe('build-utils', () => {
   test('getVersionFromArtifact returns correct version', () => {
     expect(getVersionFromArtifact('EssentialsX-2.20.1.jar')).toBe('2.20.1');
     expect(
-      getVersionFromArtifact('EssentialsX-2.20.1-dev+123-abcdef.jar')
+      getVersionFromArtifact('EssentialsX-2.20.1-dev+123-abcdef.jar'),
     ).toBe('2.20.1-dev+123-abcdef');
-    expect(
-      getVersionFromArtifact('EssentialsXChat-2.19.0-beta+5.jar')
-    ).toBe('2.19.0-beta+5');
+    expect(getVersionFromArtifact('EssentialsXChat-2.19.0-beta+5.jar')).toBe(
+      '2.19.0-beta+5',
+    );
     expect(getVersionFromArtifact('EssentialsX.jar')).toBeUndefined();
   });
 
   test('getModuleIdFromArtifact parses module id', () => {
     expect(getModuleIdFromArtifact('EssentialsX-2.19.0.jar')).toBe('core');
-    expect(getModuleIdFromArtifact('EssentialsXSpawn-2.19.0.jar')).toBe('spawn');
-    expect(getModuleIdFromArtifact('EssentialsXProtect-2.19.0.jar')).toBe('protect');
+    expect(getModuleIdFromArtifact('EssentialsXSpawn-2.19.0.jar')).toBe(
+      'spawn',
+    );
+    expect(getModuleIdFromArtifact('EssentialsXProtect-2.19.0.jar')).toBe(
+      'protect',
+    );
     expect(getModuleIdFromArtifact('RandomPlugin-1.0.jar')).toBe('core');
   });
 
   test('getCommitIdFromArtifact extracts commit id', () => {
     expect(
-      getCommitIdFromArtifact('EssentialsX-2.19.0-dev+12-abcdef.jar')
+      getCommitIdFromArtifact('EssentialsX-2.19.0-dev+12-abcdef.jar'),
     ).toBe('abcdef');
     expect(
-      getCommitIdFromArtifact('EssentialsXChat-2.19.0-rc+3-1234567.jar')
+      getCommitIdFromArtifact('EssentialsXChat-2.19.0-rc+3-1234567.jar'),
     ).toBe('1234567');
     expect(getCommitIdFromArtifact('EssentialsX-2.19.0.jar')).toBeUndefined();
   });
