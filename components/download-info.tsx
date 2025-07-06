8f40c3914877977dad5211257004b3e22ec0d230
JRoy <10731363+JRoy@users.noreply.github.com>
2025-07-06 10:44:53 -0700

    update info sidebar

--- components/download-info.tsx
+++ components/download-info.tsx
@@ -4,23 +4,23 @@ export default function DownloadInfo() {
   return (
     <div className='border-l-4 border-blue-500 p-4 not-dark:bg-blue-50 md:w-72'>
       <Text>
-        EssentialsX officially supports the Spigot and <b>Paper</b>{' '}
-        (recommended) server software running the following Minecraft versions:
+        EssentialsX officially supports the <b>Paper</b> server software running
+        the following Minecraft versions:
       </Text>
       <List spacing='xs' size='sm' mt='sm'>
         <ListItem>
-          <Text fw={700}>✅ 1.21.4 and 1.20.6</Text>
           <Text>
-            EssentialsX actively develops against and supports these versions.
+            <b>✅ 1.21.7</b> - EssentialsX actively develops against and
+            supports these versions.
           </Text>
         </ListItem>
         <ListItem>
-          <Text fw={700}>
-            ⚠️ 1.8.8, 1.9.4, 1.10.2, 1.11.2, 1.12.2, 1.13.2, 1.14.4, 1.15.2,
-            1.16.5, 1.17.1, 1.18.2, and 1.19.4
-          </Text>
           <Text>
-            These versions are still supported, but are not a priority for us,
+            <b>
+              ⚠️ 1.8.8, 1.9.4, 1.10.2, 1.11.2, 1.12.2, 1.13.2, 1.14.4, 1.15.2,
+              1.16.5, 1.17.1, 1.18.2, 1.19.4, and 1.20.6
+            </b>{' '}
+            - These versions are still supported, but are not a priority for us,
             and may be dropped in a future release.
           </Text>
         </ListItem>
